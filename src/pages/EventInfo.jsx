4f17ce590c4948fc71473353bf2593805c1a688c
lenovo <ihozomerveille@gmail.com>
2026-03-11 10:19:31 +0200

    addes files in pages folder

---
+++ src/pages/EventInfo.jsx
@@ -0,0 +1,5 @@
+function EventInfo() {
+  return <h1>Event Information Page</h1>;
+}
+
+export default EventInfo;
---
+++ src/pages/SubmittedRegistration.jsx
@@ -0,0 +1,5 @@
+function SubmittedRegistration() {
+  return <h1>Submitted Registration</h1>;
+}
+
+export default SubmittedRegistration;
