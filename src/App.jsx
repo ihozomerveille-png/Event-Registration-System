967b84c6e21340b3f8565d3371361ac32eb0e20b
lenovo <ihozomerveille@gmail.com>
2026-03-11 09:51:37 +0200

    fixing early errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import EventInfo from "./pages/EventInfo";
+import RegistrationForm from "./pages/RegistrationForm";
+import Review from "./pages/Review";
+import Confirmation from "./pages/Confirmation";
+import SubmittedRegistration from "./pages/SubmittedRegistration";
+
 function App() {
   return (
-    <div className="text-4xl font-bold text-blue-600">
-      Tailwind is working
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<EventInfo />} />
+      <Route path="/register" element={<RegistrationForm />} />
+      <Route path="/review" element={<Review />} />
+      <Route path="/confirmation" element={<Confirmation />} />
+      <Route path="/submitted" element={<SubmittedRegistration />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
